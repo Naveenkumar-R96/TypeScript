64901c3c8ff1e3eb64a062592290ec2e30558d31
NAVEEN KUMAR <nk946432@gmail.com>
2024-12-23 17:57:29 +0530

    started a typscript course

---
+++ Typescript/Annotation/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let myName = 'Naveenkumar';
+console.log(myName);
---
+++ Typescript/Annotation/index.ts
@@ -0,0 +1,16 @@
+//string
+
+let myName: string ='welcome to the typescript world'
+myName='Naveen Kumar'
+console.log(myName)
+
+//number
+
+let number : Number=3;
+console.log(number)
+
+//boolean
+
+let boolean:Boolean=false
+boolean=true;111
+console.log(boolean)
