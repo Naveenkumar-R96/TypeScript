51b1bede46abe494221b6434937041fa7894f68e
NAVEEN KUMAR <nk946432@gmail.com>
2025-01-04 17:52:58 +0530

    refactor: update generic functions and add examples for better clarity

--- Typescript-course/Generic-function/index.ts
+++ Typescript-course/Generic-function/index.ts
@@ -1,4 +1,4 @@
-function printNumber(item: number, defaultValue: number) {
+/* function printNumber(item: number, defaultValue: number) {
   return [item, defaultValue];
 }
 function printString(item: string, defaultValue: string) {
@@ -36,3 +36,61 @@ console.log(dog1)
  let aasdf:string;
 
  const dogi=asdf
+
+
+  */
+
+function printNumber(item: number, defaultValue: number): [number, number] {
+  return [item, defaultValue];
+}
+
+const vanakam = printNumber(12, 34);
+console.log(vanakam);
+
+const printe = (one: number, two: string) => {
+  let cad = one + two;
+  console.log(cad);
+};
+printe(12, "sdf");
+
+const creatingFunction = <Type>(item: Type, defaultValue: Type) => {
+  console.log("its working");
+  console.log(item, defaultValue);
+};
+
+creatingFunction<number>(12, 34);
+creatingFunction<string>("sdf", "sdf");
+creatingFunction<boolean>(true, false);
+
+type Dog = {
+  namee: string;
+  bread: string;
+};
+
+function funct(
+  { namee, bread }: Dog,
+  { namee: defaultName, bread: defaultBread }: Dog
+) {
+  console.log(namee, bread, defaultName, defaultBread);
+}
+funct({ namee: "buddy", bread: "lab" }, { namee: "default", bread: "unknown" });
+
+function getRandom<T>(obj: { [key: string]: T }): {
+  key: string;
+  value: T;
+} {
+  const keys = Object.keys(obj);
+  const randomkey = keys[Math.floor(Math.random() * keys.length)];
+  return { key: randomkey, value: obj[randomkey] };
+}
+
+const stringoj = { a: "a", b: "b", c: "c" };
+const res = getRandom<string>(stringoj);
+
+function filterArray<T>(array: T[], condition: (item: T) => boolean) {
+return array.filter((item) => condition(item))
+}
+const numberArray=[1,2,3,4,5,6,7,8,9]
+
+const evenNumber=filterArray<number>(numberArray,(item)=>item%2===0)
+console.log(evenNumber)
