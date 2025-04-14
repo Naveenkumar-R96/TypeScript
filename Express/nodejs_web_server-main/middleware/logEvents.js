ec79fef21dd439db74bca1e684999a6d5e7662d4
NAVEEN KUMAR <nk946432@gmail.com>
2025-04-14 20:27:25 +0530

    almost completed

--- Express/nodejs_web_server-main/middleware/logEvents.js
+++ Express/nodejs_web_server-main/middleware/logEvents.js
@@ -5,19 +5,25 @@ const fs = require('fs');
 const fsPromises = require('fs').promises;
 const path = require('path');
 
-const logEvents = async (message, logName) => {
+const logEvents = async (message,logName) => {
     const dateTime = `${format(new Date(), 'yyyyMMdd\tHH:mm:ss')}`;
     const logItem = `${dateTime}\t${uuid()}\t${message}\n`;
 
     try {
-        if (!fs.existsSync(path.join(__dirname, 'logs'))) {
-            await fsPromises.mkdir(path.join(__dirname, 'logs'));
+        if (!fs.existsSync(path.join(__dirname,'..', 'logs'))) {
+            await fsPromises.mkdir(path.join(__dirname,'..', 'logs'));
         }
 
-        await fsPromises.appendFile(path.join(__dirname, 'logs', logName), logItem);
+        await fsPromises.appendFile(path.join(__dirname, '..','logs',logName), logItem);
     } catch (err) {
         console.log(err);
     }
 }
 
-module.exports = logEvents;
+const logger=(req,res,next)=>{
+    logEvents(`${req.method}\t${req.url}\t${req.headers.origin},'reqLog.txt'`);
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+module.exports ={ logEvents,logger};
--- Express/nodejs_web_server-main/web_server.js
+++ Express/nodejs_web_server-main/web_server.js
@@ -2,29 +2,40 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const PORT = 3000;
-const logEvents = require('./middleware/logEvents');
+const { logger } = require('./middleware/logEvents');
+const { log } = require('console');
+const cors = require('cors');
+const errorHandler = require('./middleware/errors');
 
-app.use((req,res,next)=>{
-    console.log(`${req.method} ${req.url}`);
-    next();
-})
+const whiteList = ['https://www.google.com', 'http://localhost:3000', 'http://127.0.0.1:5500'];
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (whiteList.indexOf(origin) !== -1 || !origin) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    optionsSuccessStatus: 200
+}
 
+app.use('/', require('./Routes/root'))
+app.use(cors(corsOptions));
+app.use(logger)
+app.use(cors())
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(express.static(path.join(__dirname, './public')));
+app.use('/', express.static(path.join(__dirname, './public')));
+app.use('/subdir', express.static(path.join(__dirname, './public')));
 
-app.get(['/', '/index.html','/index'], (req, res) => {
-    res.sendFile(path.join(__dirname, 'views', 'index.html'));
-});
 
-app.get(['/new-page','/new-page.html'], (req, res)=>{
-    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
-})
-app.get(['/old-page','/old-page.html'], (req, res)=>{
-    res.redirect(301, '/new-page.html');
-})
+app.use('/subdir', require('./Routes/subdir'));
+app.use('/employee',require('./Routes/api/employee'));
+
 app.use((req, res) => {
     res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
+
+app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
