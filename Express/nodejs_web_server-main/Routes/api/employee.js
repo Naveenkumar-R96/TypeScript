c36fe5e551de4c52bb0590349e0460c37b31c2b3
NAVEEN KUMAR <nk946432@gmail.com>
2025-05-05 20:49:07 +0530

    recaped

--- Express/nodejs_web_server-main/Routes/api/employee.js
+++ Express/nodejs_web_server-main/Routes/api/employee.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-
 const employeeController = require('../../controllers/employeecontro');
-const e = require('express');
+const employeeData = require('../../data/employee.json');
 
-router.route('/')
+// Routes for employees
+router
+    .route('/')
     .get(employeeController.getEmployees)
     .post(employeeController.createEmployee)
     .put(employeeController.updateEmployee)
     .delete(employeeController.deleteEmployee);
-    router.route('/:id').get(employeeController.getEmployeeById)    
+
+router
+    .route('/:id')
+    .get(employeeController.getEmployeeById);
+
 module.exports = router;
